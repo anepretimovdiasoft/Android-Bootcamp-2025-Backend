10425e5c7505e4e3c79fc3d0f767ea71983a6c57
Petr Rudichev <petrrudichev@gmail.com>
2025-01-30 16:30:19 +0400

    feat: added liquibase migration

--- src/main/java/com/example/bootcamp/App.java
+++ src/main/java/com/example/bootcamp/App.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+        // https://www.geeksforgeeks.org/how-to-send-images-in-spring-boot-without-using-servlet-and-redundant-jsp-codes/
     }
 }
