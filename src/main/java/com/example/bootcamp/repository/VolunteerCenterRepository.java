808f326f151e25f5d008d1f129907fd6a6d843e5
truettwo <fedor.ivanov2009@mail.ru>
2025-02-03 18:04:47 +0300

    Boot

--- src/main/java/com/example/bootcamp/repository/VolunteerCenterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bootcamp.repository;
-
-import com.example.bootcamp.model.VolunteerCenter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VolunteerCenterRepository extends JpaRepository<VolunteerCenter, Long> {
-}
